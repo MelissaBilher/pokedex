c3344fb36d9af5bba8d2c8ac093303ff2c51855e
Melissa Bilher <mbilherr@gmail.com>
2022-04-25 19:44:26 -0300

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,19 @@
+ /* Quando clicar no pokemon escolhido na listagem temos que esconder o cartão aberto e mostrar o cartão correspondente ao que foi selecionado. 
+
+
+ Remover a classe aberto só do card que está aberto.
+ Ao clicar em um pokemon da listagem pegamos o id desse card para saber qual mostrar.
+ Remover a classe ativo que marca o pokemon selecionado.
+ Adicionar a classe ativo no item da lista selecionada.
+ */
+
+// Precisamos trabalhar com dois elementos (listagem e cartão pokemon - criar duas variáveis). Evento de clique feito pelo usuário na listagem. 
+ const listingPokemon = document.querySelectorAll('.Pokemon')
+ const pokemonsCard = document.querySelectorAll('.
+  card')
+
+ listingPokemon.forEach(Pokemon => {
+  //Evento de clique feito pelo usuário na listagem.  
+  pokemon.addEventListener('click', () =>  )
+ })
+
