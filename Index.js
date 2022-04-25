56ced2a6a0c574bb1ef47f10283236bf95701269
Melissa Bilher <mbilherr@gmail.com>
2022-04-25 19:59:07 -0300

    Update Index.js
    Retirada a opção de forçava o card do Pikachu ficar aberto.

--- Index.js
+++ Index.js
@@ -1,7 +1,5 @@
  /* Quando clicar no pokemon escolhido na listagem temos que esconder o cartão aberto e mostrar o cartão correspondente ao que foi selecionado. 
 
-
- Remover a classe aberto só do card que está aberto.
  Ao clicar em um pokemon da listagem pegamos o id desse card para saber qual mostrar.
  Remover a classe ativo que marca o pokemon selecionado.
  Adicionar a classe ativo no item da lista selecionada.
@@ -9,11 +7,18 @@
 
 // Precisamos trabalhar com dois elementos (listagem e cartão pokemon - criar duas variáveis). Evento de clique feito pelo usuário na listagem. 
  const listingPokemon = document.querySelectorAll('.Pokemon')
- const pokemonsCard = document.querySelectorAll('.
-  card')
+ const pokemonsCard = document.querySelectorAll('.card')
 
  listingPokemon.forEach(Pokemon => {
   //Evento de clique feito pelo usuário na listagem.  
-  pokemon.addEventListener('click', () =>  )
+  
+ Pokemon.addEventListener('click', () => {
+    //Ao clicar em um pokemon da listagem pegamos o id desse card para saber qual mostrar.
+    const idPokemonSelect = Pokemon.attributes.id.value 
+
+    const cardPokemonOpen = document.getElementById('card-bulbasaur')
+
+    cardPokemonOpen.classList.add('open')
+  })
  })
 
